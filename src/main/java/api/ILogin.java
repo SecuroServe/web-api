c4d938621469751918921562edbca4b6604b09dc
Jandie <eidnaj@live.nl>
2017-03-13 12:39:04 +0100

    Added javadoc to ILogin inteface

--- src/main/java/api/ILogin.java
+++ src/main/java/api/ILogin.java
@@ -1,12 +1,23 @@
 package api;
 
 import library.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by Jandie on 13-3-2017.
  */
 public interface ILogin {
-    String getToken(@RequestParam(value="username") String username,
+
+    /**
+     * Creates and returns a new token for a user if username and password
+     * are correct.
+     * @param username The username of the user.
+     * @param password The password of the user.
+     * @return The token of the user.
+     */
+    @RequestMapping(value = "/api/login", method = RequestMethod.POST)
+    String login(@RequestParam(value="username") String username,
                     @RequestParam(value="password") String password);
 }
