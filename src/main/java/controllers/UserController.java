c041328b5581173e98ed1e9a924762f6be7d3e21
Yannic Riesebos <zebybez@surfspot.nu>
2017-03-20 14:33:17 +0100

    added StatusType enum, working on CalamityController

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -3,6 +3,7 @@ package controllers;
 import api.ConfirmationMessage;
 import api.IUser;
 import dataRepo.UserRepo;
+import enums.StatusType;
 import library.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -73,14 +74,14 @@ public class UserController implements IUser{
             User user = new UserRepo().register(userTypeId,
                     calamityAssigneeId, buildingId, username, password, email, city);
 
-            return new ConfirmationMessage("Succes", "User added!", user);
+            return new ConfirmationMessage(StatusType.SUCCES, "User added!", user);
 
         } catch (NoSuchAlgorithmException | SQLException e) {
             Logger.getLogger(UserController.class.getName()).log(Level.SEVERE,
                     "User addition failed!", e);
         }
 
-        return new ConfirmationMessage("Error",
+        return new ConfirmationMessage(StatusType.ERROR,
                 "User addition failed!", null);
     }
 
@@ -119,13 +120,13 @@ public class UserController implements IUser{
         try {
             new UserRepo().deleteUser(id);
 
-            return new ConfirmationMessage("Succes", "User deleted!", null);
+            return new ConfirmationMessage(StatusType.SUCCES, "User deleted!", null);
         } catch (SQLException e) {
             Logger.getLogger(UserController.class.getName()).log(Level.SEVERE,
                     "User deletion failed!", e);
         }
 
-        return new ConfirmationMessage("Error",
+        return new ConfirmationMessage(StatusType.ERROR,
                 "User deletion failed!", null);
     }
 }
