eee8910e4d6a3fef160604db5f233b51d3828f9c
Jandie <eidnaj@live.nl>
2017-03-20 09:35:11 +0100

    Database class added

---
+++ src/main/java/dataRepo/QueryType.java
@@ -0,0 +1,19 @@
+package dataRepo;
+
+/**
+ * Enum which sets the type of query.
+ *
+ * @author Jandie
+ */
+public enum QueryType {
+
+    /**
+     * Type of query.
+     */
+    QUERY,
+
+    /**
+     * A non query type.
+     */
+    NON_QUERY
+}
