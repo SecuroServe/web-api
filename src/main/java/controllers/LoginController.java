2085b26fd13c1e07c086932e2a897778a1f87a84
Jandie <eidnaj@live.nl>
2017-03-20 11:40:58 +0100

    LoginController updated

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -5,6 +5,13 @@ import dataRepo.UserRepo;
 import org.springframework.web.bind.annotation.RequestMapping;
         import org.springframework.web.bind.annotation.RequestParam;
         import org.springframework.web.bind.annotation.RestController;
+import utils.HashUtil;
+
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by Jandie on 13-3-2017.
@@ -15,6 +22,12 @@ public class LoginController{
     @RequestMapping("/login")
     public String login(@RequestParam(value = "username", defaultValue="") String username,
                         @RequestParam(value = "password", defaultValue="") String password) {
-        return new UserRepo().login(username, password);
+        try {
+            return new UserRepo().login(username, password);
+        } catch (SQLException | ParseException | NoSuchAlgorithmException e) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, "Login failed!", e);
+
+            return null;
+        }
     }
 }
