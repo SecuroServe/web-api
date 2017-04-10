c868044cd7f0b80c3186d39867a406eba780f87c
Mans,Michel M.M.T.M <michel.mans@student.fontys.nl>
2017-04-10 11:20:40 +0200

    Started to implement the database queries from Calamity and Location

--- src/main/java/library/Location.java
+++ src/main/java/library/Location.java
@@ -5,9 +5,10 @@ package library;
  * Created by Jandie on 13-3-2017.
  */
 public class Location {
-    private long longitude;
-    private long latitude;
-    private long radius;
+    private float longitude;
+    private float latitude;
+    private float radius;
+    private int id;
 
     /***
      * the constructor for a new location .
@@ -15,33 +16,37 @@ public class Location {
      * @param latitude the latutude of the location.
      * @param radius the radius of the location, can be 0.
      */
-    public Location(long longitude, long latitude, long radius) {
+    public Location(float latitude, float longitude, float radius) {
         this.longitude = longitude;
         this.latitude = latitude;
         this.radius = radius;
     }
 
-    public long getLongitude() {
-        return longitude;
+    public float getLongitude() {
+        return this.longitude;
     }
 
     public void setLongitude(long longitude) {
         this.longitude = longitude;
     }
 
-    public long getLatitude() {
-        return latitude;
+    public float getLatitude() {
+        return this.latitude;
     }
 
     public void setLatitude(long latitude) {
         this.latitude = latitude;
     }
 
-    public long getRadius() {
-        return radius;
+    public float getRadius() {
+        return this.radius;
     }
 
     public void setRadius(long radius) {
         this.radius = radius;
     }
+
+    public void setId(int id){ this.id = id; }
+
+    public int getId() { return this.id; }
 }
