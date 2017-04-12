37a2c5161657d47ac0db3f1a1df7f819ba017e0c
Jandie Hendriks <eidnaj@live.nl>
2017-04-12 09:52:14 +0200

    Added permissions

--- src/main/java/securoserve/library/Permission.java
+++ src/main/java/securoserve/library/Permission.java
@@ -3,5 +3,12 @@ package securoserve.library;
 /**
  * Created by Jandie on 13-3-2017.
  */
-public class Permission {
+public enum Permission {
+    CALAMITY_ADD,
+    CALAMITY_UPDATE,
+    CALAMITY_GET,
+    CALAMITY_DELETE,
+    USER_REGISTER,
+    USER_DELETE,
+    USER_UPDATE
 }
