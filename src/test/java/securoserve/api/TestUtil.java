6c5ae44398d1246e3b3746804744a3a4a2d1dd12
Jandie Hendriks <eidnaj@live.nl>
2017-04-12 14:58:58 +0200

    Fixed unittests

--- src/test/java/securoserve/api/TestUtil.java
+++ src/test/java/securoserve/api/TestUtil.java
@@ -3,7 +3,8 @@ package securoserve.api;
 import org.junit.Assert;
 import securoserve.api.controllers.LoginController;
 import securoserve.api.controllers.UserController;
-import securoserve.library.Calamity;
+import securoserve.api.datarepo.Database;
+import securoserve.api.datarepo.UserRepo;
 import securoserve.library.User;
 
 /**
@@ -15,20 +16,24 @@ public class TestUtil {
     public static final String EMAIL = "testuser789987@test123weqr456.nl";
     public static final String CITY = "Amsterdam";
 
-    public static void deleteTempUser(User user) {
-        UserController uc = new UserController();
+    public static void deleteTempUser(User user) throws Exception {
         LoginController lc = new LoginController();
+        Database db = new Database();
+        UserRepo userRepo = new UserRepo(db);
 
-        uc.deleteUser(user.getToken(), user.getId());
+        userRepo.deleteUser(user.getId());
 
         String token = lc.login(USERNAME, PASSWORD);
         Assert.assertEquals(null, token);
     }
 
-    public static User createTempUser() {
+    public static User createTempUser() throws Exception {
         UserController uc = new UserController();
-        User user = (User) uc.addUser(1, -1,
-                USERNAME, PASSWORD, EMAIL, CITY, "").getReturnObject();
+        Database db = new Database();
+        UserRepo userRepo = new UserRepo(db);
+
+        User user = userRepo.register(1, -1,
+                USERNAME, PASSWORD, EMAIL, CITY);
 
         user = uc.getUser(user.getToken());
         Assert.assertEquals(USERNAME, user.getUsername());
--- src/test/java/securoserve/api/controllers/LoginControllerTest.java
+++ src/test/java/securoserve/api/controllers/LoginControllerTest.java
@@ -3,7 +3,6 @@ package securoserve.api.controllers;
 import org.junit.Assert;
 import org.junit.Test;
 import securoserve.api.TestUtil;
-import securoserve.api.interfaces.ConfirmationMessage;
 import securoserve.library.User;
 
 /**
