ecfa63d7187bdb8e53ef2d5e33e099d12e61aecd
Jandie Hendriks <eidnaj@live.nl>
2017-04-12 12:50:15 +0200

    Implemented usertype

--- src/main/java/securoserve/library/UserType.java
+++ src/main/java/securoserve/library/UserType.java
@@ -1,10 +1,23 @@
 package securoserve.library;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Jandie on 13-3-2017.
  */
 public class UserType {
+
+    private String name;
     private List<Permission> permissions;
+
+    public UserType(String name) {
+        this.name = name;
+
+        permissions =  new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
 }
