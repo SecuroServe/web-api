650c868637d8789f0db9a63f51c6cec73ce5cb76
jandie <eidnaj@live.nl>
2017-05-02 14:05:51 +0200

    Updated mappings in API controllers

--- src/main/java/securoserve/api/controllers/CalamityController.java
+++ src/main/java/securoserve/api/controllers/CalamityController.java
@@ -1,5 +1,6 @@
 package securoserve.api.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import securoserve.api.datarepo.database.Database;
@@ -33,6 +34,7 @@ public class CalamityController implements ICalamity {
      * @return A list with all current calamities.
      */
     @Override
+    @RequestMapping("/allcalamity")
     public List<Calamity> allCalamity() {
         return (List<Calamity>) calamityLogic.allCalamity().getReturnObject();
     }
@@ -45,6 +47,7 @@ public class CalamityController implements ICalamity {
      * @return A single calamity by id.
      */
     @Override
+    @RequestMapping("/calamitybyid")
     public Calamity calamityById(@RequestParam(value = "token") String token,
                                  @RequestParam(value = "id") int id) {
         return (Calamity) calamityLogic.getCalamity(token, id).getReturnObject();
@@ -61,6 +64,7 @@ public class CalamityController implements ICalamity {
      * also containing the new calamity.
      */
     @Override
+    @RequestMapping("/addcalamity")
     public ConfirmationMessage addCalamity(@RequestParam(value = "token") String token,
                                            @RequestParam(value = "title") String title,
                                            @RequestParam(value = "message") String message,
@@ -81,6 +85,7 @@ public class CalamityController implements ICalamity {
      * @return Confirmation message with feedback about the update.
      */
     @Override
+    @RequestMapping("/updatecalamity")
     public ConfirmationMessage updateCalamity(@RequestParam(value = "token") String token,
                                               @RequestParam(value = "id") int id,
                                               @RequestParam(value = "name") String name,
@@ -92,6 +97,7 @@ public class CalamityController implements ICalamity {
     }
 
     @Override
+    @RequestMapping("/deletecalamity")
     public ConfirmationMessage deleteCalamity(String token, int id) {
         return calamityLogic.deleteCalamity(token, id);
     }
@@ -105,6 +111,7 @@ public class CalamityController implements ICalamity {
      * @return Confirmation message with feedback about the addition.
      */
     @Override
+    @RequestMapping("/addcalamityassignee")
     public ConfirmationMessage addCalamityAssignee(String token, int calamityId, int userId) {
         return calamityLogic.addCalamityAssignee(token, calamityId, userId);
     }
@@ -118,6 +125,7 @@ public class CalamityController implements ICalamity {
      * @return Confirmation message with feedback about the deletion.
      */
     @Override
+    @RequestMapping("/deletecalamityassignee")
     public ConfirmationMessage deleteCalamityAssignee(String token, int calamityId, int userId) {
         return calamityLogic.deleteCalamityAssignee(token, calamityId, userId);
     }
