441d88cef461b3d625c0b2195571dbecd5ca52b7
jandie <eidnaj@live.nl>
2017-05-29 13:11:33 +0200

    Optimized imports in Application

--- api/src/main/java/controllers/Application.java
+++ api/src/main/java/controllers/Application.java
@@ -1,15 +1,7 @@
 package controllers;
 
-import com.google.gson.JsonObject;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import utils.FCMHelper;
 
 import java.io.IOException;
 
