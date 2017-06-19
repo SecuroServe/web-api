108e5fa8b4f934e16975db7b1112e4d7d0f909e3
Jandie Hendriks <eidnaj@live.nl>
2017-06-19 10:42:56 +0200

    Added unittests class for SocialController

---
+++ api/src/test/java/controllers/SocialControllerTest.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import org.junit.Test;
+
+/**
+ * Created by Jandie on 19-Jun-17.
+ */
+public class SocialControllerTest {
+    @Test
+    public void getSocialPosts() throws Exception {
+
+    }
+}
