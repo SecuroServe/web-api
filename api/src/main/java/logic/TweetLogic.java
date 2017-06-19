d416fb01018e4e7d4e14ee4a4be0a6c1b2a69e37
Jandie Hendriks <eidnaj@live.nl>
2017-06-19 10:29:58 +0200

    Added classes en methods to implement ITweet in api

---
+++ api/src/main/java/logic/TweetLogic.java
@@ -0,0 +1,19 @@
+package logic;
+
+import interfaces.ConfirmationMessage;
+
+/**
+ * Created by Jandie on 19-Jun-17.
+ */
+public class TweetLogic {
+    /**
+     * Find tweets based on key words.
+     *
+     * @param token    The authentication token.
+     * @param keyWords String with different keywords separated by spaces.
+     * @return List of found tweet objects.
+     */
+    public ConfirmationMessage getTweets(String token, String keyWords) {
+        return null;
+    }
+}
