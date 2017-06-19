cc3e5bc75577db8f8ee820ecd60899046d534254
Jandie Hendriks <eidnaj@live.nl>
2017-06-19 10:24:48 +0200

    Tweet object added to library

---
+++ library/src/main/java/library/Tweet.java
@@ -0,0 +1,42 @@
+package library;
+
+import java.util.Date;
+
+/**
+ * Created by Jandie on 19-Jun-17.
+ */
+public class Tweet {
+    String username;
+    String tweetMessage;
+    Date createdDate;
+
+    public Tweet(String username, String tweetMessage, Date createdDate) {
+        this.username = username;
+        this.tweetMessage = tweetMessage;
+        this.createdDate = createdDate;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTweetMessage() {
+        return tweetMessage;
+    }
+
+    public void setTweetMessage(String tweetMessage) {
+        this.tweetMessage = tweetMessage;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+}
